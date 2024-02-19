21b9f90a27f90966c88053764745cd871ffa3ce1
kazu0429 <k.shouhi0412@i.softbank.jp>
2024-02-20 02:32:22 +0900

    add search

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,8 @@
-import type { LinksFunction } from "@remix-run/node";
+import { useEffect } from "react";
+import type {
+  LinksFunction,
+  LoaderFunctionArgs
+} from "@remix-run/node";
 import { json, redirect } from "@remix-run/node";
 import appStyleHref from "./app.css";
 import { createEmptyContact, getContacts } from "./data";
@@ -14,15 +18,20 @@ import {
   ScrollRestoration,
   useLoaderData,
   useNavigation,
+  useSubmit
 } from "@remix-run/react";
 
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: appStyleHref },
 ]
 
-export const loader = async () => {
-  const contacts = await getContacts();
-  return json({ contacts })
+export const loader = async ({
+  request
+}: LoaderFunctionArgs) => {
+  const url = new URL(request.url);
+  const q = url.searchParams.get("q");
+  const contacts = await getContacts(q);
+  return json({ contacts, q });
 }
 
 export const action = async () => {
@@ -31,8 +40,17 @@ export const action = async () => {
 }
 
 export default function App() {
-  const { contacts } = useLoaderData<typeof loader>();
+  const { contacts, q } = useLoaderData<typeof loader>();
   const navigation = useNavigation();
+  const submit = useSubmit();
+  const searching = navigation.location && new URLSearchParams(navigation.location.search).has("q");
+
+  useEffect(() => {
+    const searchField = document.getElementById("q");
+    if(searchField instanceof HTMLInputElement){
+      searchField.value = q || ""
+    }
+  }, [q])
 
   return (
     <html lang="en">
@@ -46,15 +64,25 @@ export default function App() {
         <div id="sidebar">
           <h1>Remix Contacts</h1>
           <div>
-            <Form id="search-form" role="search">
+            <Form
+            id="search-form"
+            onChange={(event) =>{
+              const isFIrstSearch = q === null;
+              submit(event.currentTarget, {
+                replace: !isFIrstSearch
+              })
+            }}
+            role="search">
               <input
                 id="q"
+                className={searching ? "loading" : ""}
+                defaultValue={q || ""}
                 aria-label="Search contacts"
                 placeholder="Search"
                 type="search"
                 name="q"
               />
-              <div id="search-spinner" aria-hidden hidden={true} />
+              <div id="search-spinner" aria-hidden hidden={!searching} />
             </Form>
             <Form method="post">
               <button type="submit">New</button>
@@ -98,7 +126,9 @@ export default function App() {
         </div>
         <div
           className={
-            navigation.state === "loading" ? "loading" : ""
+            navigation.state === "loading" && !searching
+              ? "loading"
+              : ""
           }
           id="detail"
         >
--- app/routes/contacts.$contactId.tsx
+++ app/routes/contacts.$contactId.tsx
@@ -1,21 +1,39 @@
-import type { LoaderFunctionArgs } from "@remix-run/node";
+import type {
+    LoaderFunctionArgs,
+    ActionFunctionArgs
+} from "@remix-run/node";
 import invariant from "tiny-invariant";
-import { Form, useLoaderData } from "@remix-run/react";
+import {
+    Form,
+    useLoaderData,
+    useFetcher
+} from "@remix-run/react";
 import { json } from "@remix-run/node";
 import type { FunctionComponent } from "react";
 
-import { getContact, type ContactRecord } from "~/data";
+import { getContact, updateContact, type ContactRecord } from "../data";
 
 export const loader = async ({
     params,
-}:LoaderFunctionArgs) => {
+}: LoaderFunctionArgs) => {
     // inbariant:値がtrueの場合は何もせず、falseの場合は第二引数に渡した値をエラーメッセージとした例外を発生
     invariant(params.contactId, "Missing contactId param");
     const contact = await getContact(params.contactId);
-    if(!contact){
-        throw new Response("Not Found", {status:404})
+    if (!contact) {
+        throw new Response("Not Found", { status: 404 })
     }
-    return json({contact});
+    return json({ contact });
+}
+
+export const action = async ({
+    params,
+    request,
+}: ActionFunctionArgs) => {
+    invariant(params.contactId, "Missing contactId param");
+    const formData = await request.formData();
+    return updateContact(params.contactId, {
+        favorite: formData.get("favorite") === "true",
+    });
 }
 
 export default function Contact() {
@@ -81,19 +99,24 @@ export default function Contact() {
 const Favorite: FunctionComponent<{
     contact: Pick<ContactRecord, "favorite">;
 }> = ({ contact }) => {
-    const favorite = contact.favorite;
+    const fetcher = useFetcher();
+    const favorite = fetcher.formData
+    ? fetcher.formData.get("favorite") === "true"
+    : contact.favorite;
 
     return (
-        <Form method="post">
+        <fetcher.Form method="post">
             <button
                 aria-label={
                     favorite
                         ? "Remove from favorites"
                         : "Add to favorites"
                 }
+                name="favorite"
+                value={favorite ? "false" : "true"}
             >
                 {favorite ? "★" : "☆"}
             </button>
-        </Form>
+        </fetcher.Form>
     )
 }
